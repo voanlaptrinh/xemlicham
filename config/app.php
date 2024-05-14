18bd9756bf490f3b93729578bfe58169e13b23e7
khanh <voanlaptrinh@gmail.com>
2024-05-14 16:45:04 +0700

    mian

--- config/app.php
+++ config/app.php
@@ -181,7 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        'Barryvdh\Debugbar\ServiceProvider',
         /*
          * Package Service Providers...
          */
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',
     ])->toArray(),
 
 ];
